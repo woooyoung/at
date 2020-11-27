2322afb4a6474fab6da7fcd2a6f79f62501f6b5a
woooyoung <axdsw121@gmail.com>
2020-11-27 21:00:38 +0900

    20201127 HomeController Basic

---
+++ at/src/test/java/com/sbs/cwy/at/AtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbs.cwy.at;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
