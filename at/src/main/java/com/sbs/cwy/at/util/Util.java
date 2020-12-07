39e7dfe5011ac6e7442e521109062ff502100e82
woooyoung <axdsw121@gmail.com>
2020-12-07 15:23:27 +0900

    20201207 ArticleReply modify

--- at/src/main/java/com/sbs/cwy/at/util/Util.java
+++ at/src/main/java/com/sbs/cwy/at/util/Util.java
@@ -1,9 +1,10 @@
 package com.sbs.cwy.at.util;
 
 import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Util {
-
 	public static int getAsInt(Object object) {
 		if (object instanceof BigInteger) {
 			return ((BigInteger) object).intValue();
@@ -14,7 +15,18 @@ public class Util {
 		} else if (object instanceof String) {
 			return Integer.parseInt((String) object);
 		}
+
 		return -1;
+	}
+
+	public static Map<String, Object> getNewMapOf(Map<String, Object> oldMap, String... keys) {
+		Map<String, Object> newMap = new HashMap<>();
+
+		for (String key : keys) {
+			newMap.put(key, oldMap.get(key));
+		}
+
+		return newMap;
 	}
 
 }
