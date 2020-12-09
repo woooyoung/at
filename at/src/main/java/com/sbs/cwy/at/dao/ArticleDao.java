e73a40cb1b1022aa65700e05729a88f0e26a55bd
woooyoung <axdsw121@gmail.com>
2020-12-09 17:02:22 +0900

    20201209 Reply logic moved

--- at/src/main/java/com/sbs/cwy/at/dao/ArticleDao.java
+++ at/src/main/java/com/sbs/cwy/at/dao/ArticleDao.java
@@ -7,7 +7,6 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import com.sbs.cwy.at.dto.Article;
-import com.sbs.cwy.at.dto.Reply;
 
 @Mapper
 public interface ArticleDao {
@@ -16,14 +15,4 @@ public interface ArticleDao {
 	Article getForPrintArticleById(@Param("id") int id);
 
 	void write(Map<String, Object> param);
-
-	void writeReply(Map<String, Object> param);
-
-	List<Reply> getForPrintReplies(Map<String, Object> param);
-
-	void deleteReply(@Param("id") int id);
-
-	Reply getForPrintReplyById(@Param("id") int id);
-
-	void modifyReply(Map<String, Object> param);
 }
--- at/src/main/java/com/sbs/cwy/at/service/ArticleService.java
+++ at/src/main/java/com/sbs/cwy/at/service/ArticleService.java
@@ -5,17 +5,14 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.sbs.cwy.at.dao.ArticleDao;
 import com.sbs.cwy.at.dto.Article;
-import com.sbs.cwy.at.dto.Member;
-import com.sbs.cwy.at.dto.Reply;
-import com.sbs.cwy.at.dto.ResultData;
 import com.sbs.cwy.at.util.Util;
 
 @Service
 public class ArticleService {
+	
 	@Autowired
 	private ArticleDao articleDao;
 
@@ -35,52 +32,5 @@ public class ArticleService {
 		articleDao.write(param);
 
 		return Util.getAsInt(param.get("id"));
-	}
-
-	public int writeReply(Map<String, Object> param) {
-		articleDao.writeReply(param);
-
-		return Util.getAsInt(param.get("id"));
-	}
-
-	public List<Reply> getForPrintReplies(@RequestParam Map<String, Object> param) {
-		List<Reply> replies = articleDao.getForPrintReplies(param);
-
-		Member actor = (Member) param.get("actor");
-
-		for (Reply reply : replies) {
-			// 출력용 부가데이터를 추가한다.
-			updateForPrintInfo(actor, reply);
-		}
-
-		return replies;
-	}
-
-	private void updateForPrintInfo(Member actor, Reply reply) {
-		reply.getExtra().put("actorCanDelete", actorCanDelete(actor, reply));
-		reply.getExtra().put("actorCanModify", actorCanModify(actor, reply));
-	}
-
-	// 액터가 해당 댓글을 수정할 수 있는지 알려준다.
-	public boolean actorCanModify(Member actor, Reply reply) {
-		return actor != null && actor.getId() == reply.getMemberId() ? true : false;
-	}
-
-	// 액터가 해당 댓글을 삭제할 수 있는지 알려준다.
-	public boolean actorCanDelete(Member actor, Reply reply) {
-		return actorCanModify(actor, reply);
-	}
-
-	public void deleteReply(int id) {
-		articleDao.deleteReply(id);
-	}
-
-	public Reply getForPrintReplyById(int id) {
-		return articleDao.getForPrintReplyById(id);
-	}
-
-	public ResultData modfiyReply(Map<String, Object> param) {
-		articleDao.modifyReply(param);
-		return new ResultData("S-1", String.format("%d번 댓글을 수정하였습니다.", Util.getAsInt(param.get("id"))), param);
 	}
 }
