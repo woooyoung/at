ceb8b5caee8f070b11d4ddd6b51e9fe31bec337c
woooyoung <axdsw121@gmail.com>
2020-12-30 15:01:44 +0900

    20201230 toastuiEditor ios

--- at/src/main/resources/static/resource/common.js
+++ at/src/main/resources/static/resource/common.js
@@ -46,3 +46,9 @@ function getHtmlEncoded(raw) {
 		return '&#' + i.charCodeAt(0) + ';';
 	});
 }
+
+function iOS() {
+	return ['iPad Simulator', 'iPhone Simulator', 'iPod Simulator', 'iPad', 'iPhone', 'iPod'].includes(navigator.platform)
+		// iPad on iOS 13 detection
+		|| (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+}
